ad2aab29c1128d935d5220ce1cdb9462c54890e0
iqbalimaduddin8 <iqbalimaduddin8@gmail.com>
2024-01-28 14:02:41 +0700

    first init

---
+++ js/index.js
@@ -0,0 +1,210 @@
+function myFunction() {
+  // Get the number of dimensions you want
+  var x = document.getElementById("amount");
+
+  // Check if table was created, if has created it will remove and running the code below to create new table
+  let element = document.getElementById("myTable");
+  if (element) {
+    element.remove();
+  }
+
+  // Create the table and his attribute
+  let table = document.createElement("table");
+  table.setAttribute("id", "myTable");
+  document.getElementById("div-table").appendChild(table);
+
+  // Array used to contain potential winning lines
+  let winPossibility = [];
+  // Create row for the table
+  for (let i = 0; i < x.value; i++) {
+    let tr = document.createElement("tr");
+    table.appendChild(tr);
+
+    // Create column for the row
+    for (let j = 0; j < x.value; j++) {
+      let td = document.createElement("td");
+      tr.appendChild(td);
+
+      // Function used to find potential winning lines
+      const datas = checkCorrectLine(i, j, x.value);
+      winPossibility.push(...datas);
+ 
+      // Still have bug, when it click comp or herself it will autogenerate comp
+      // The finish have not created yet
+      td.onclick = function () {
+        let arr = clickCell(td, tr, winPossibility);
+        winPossibility = arr;
+        console.log("winPossibility", winPossibility);
+        let arr2 = compLogic(table, winPossibility);
+        winPossibility = arr2
+        console.log("winPossibility2", winPossibility);
+      };
+    }
+  }
+}
+
+function compLogic(table, winPossibility) {
+  let arrRandom = [];
+
+  //  Find cell still empty
+  let searchBool = true;
+  winPossibility?.map((row, index) => {
+    {
+      if (!row.includes(searchBool)) {
+        arrRandom.push(index);
+      }
+    }
+  });
+
+  //  Random cells that still empty to be inserted
+  let randomNumber = random(arrRandom);
+
+  //   Get index row and cell index
+  let arrCellRandom = [];
+  winPossibility[randomNumber].map((item, index) => {
+    arrCellRandom.push(index);
+  });
+
+  console.log("arrCellRandom", arrCellRandom);
+  let randomNumberCell = random(arrCellRandom);
+  console.log("randomNumberCell", randomNumberCell);
+  let cellRowArr = [];
+  console.log("winPossibility[randomNumber]", winPossibility[randomNumber]);
+  winPossibility[randomNumber].map((item, index) => {
+    console.log("index", index);
+    if (randomNumberCell == index) {
+      cellRowArr = item.split(",");
+      console.log("cellRowAr1", cellRowArr);
+      cellRowArr = cellRowArr.map((item) => {
+        return parseInt(item);
+      });
+    }
+  });
+  console.log("cellRowArr", cellRowArr);
+
+  var cell = document.getElementById("myTable").rows[cellRowArr[0]].cells;
+  cell[cellRowArr[1]].innerHTML = "O";
+  // I think there still bugs here because it reversed beetween row and cell
+  let search = cellRowArr[0] + "," +  cellRowArr[1];
+  arr = changeArray(winPossibility, search, false);
+  return arr;
+}
+
+// Function used to choose random square to be filled by the computer
+function random(numbers) {
+  return numbers[Math.floor(Math.random() * numbers.length)];
+}
+
+// Click for contain the cell
+function clickCell(td, tr, arr) {
+  if (!td.textContent) {
+    let search = tr.rowIndex + "," +  td.cellIndex;
+    arr = changeArray(arr, search, true);
+    text = document.createTextNode("X");
+    td.appendChild(text);
+    return arr;
+  } else {
+    return arr;
+  }
+}
+
+// Used to change the square contain that is filled by user with true boolean
+function changeArray(array2d, itemtofind, bool) {
+  return array2d.map((row) => {
+    return row.map((cell) => {
+      if (cell == itemtofind) {
+        return (cell = bool);
+      } else {
+        return (cell = cell);
+      }
+    });
+  });
+}
+// If you've done make the goals scenario. You can use this for computer and user.
+// When it is user turn it will change bool to be true and change the array scenario with true.
+// When user finish click it bool will be false and automatically generate X for comp.
+// if the array become true, comp automatically get the array still have neutral array, if neutral array is nothing, comp will choose else if to fill random cell.
+
+// The scenario
+// 0 +1
+// +1 +1
+// +1 0
+// +1 -1
+let cellArrDiagonal1 = [];
+function straightLineDiagonal1(row, column, lineLength) {
+  cellArrDiagonal1.push(row + "," + column);
+  //   scenario +1 +1
+  row = row + 1;
+  column = column + 1;
+
+  if (row < lineLength && column < lineLength) {
+    straightLineDiagonal1(row, column, lineLength);
+  }
+}
+
+let cellHorizontal = [];
+function straightLineHorizontal(row, column, lineLength) {
+  cellHorizontal.push(row + "," + column);
+  //  scenario +0 +1
+  row = row + 0;
+  column = column + 1;
+
+  if (row < lineLength && column < lineLength) {
+    straightLineHorizontal(row, column, lineLength);
+  }
+}
+
+let cellVertical = [];
+function straightLineVertical(row, column, lineLength) {
+  cellVertical.push(row + "," + column);
+  //  scenario +1 +0
+  row = row + 1;
+  column = column + 0;
+
+  if (row < lineLength && column < lineLength) {
+    straightLineVertical(row, column, lineLength);
+  }
+}
+
+let cellArrDiagonal2 = [];
+function straightLineDiagonal2(row, column, lineLength) {
+  cellArrDiagonal2.push(row + "," + column);
+  //  scenario +1 -1
+  row = row + 1;
+  column = column - 1;
+  if (row < 0 || column < 0) {
+    return;
+  }
+
+  if (row < lineLength && column < lineLength) {
+    straightLineDiagonal2(row, column, lineLength);
+  }
+}
+
+// Check the winning line
+function checkCorrectLine(i, j, lineLength) {
+  let tableArr = [];
+  cellArrDiagonal1 = [];
+  straightLineDiagonal1(i, j, lineLength);
+  if (cellArrDiagonal1.length == lineLength) {
+    tableArr.push(cellArrDiagonal1);
+  }
+  cellHorizontal = [];
+  straightLineHorizontal(i, j, lineLength);
+  if (cellHorizontal.length == lineLength) {
+    tableArr.push(cellHorizontal);
+  }
+
+  cellVertical = [];
+  straightLineVertical(i, j, lineLength);
+  if (cellVertical.length == lineLength) {
+    tableArr.push(cellVertical);
+  }
+  cellArrDiagonal2 = [];
+  straightLineDiagonal2(i, j, lineLength);
+  if (cellArrDiagonal2.length == lineLength) {
+    tableArr.push(cellArrDiagonal2);
+  }
+
+  return tableArr;
+}
